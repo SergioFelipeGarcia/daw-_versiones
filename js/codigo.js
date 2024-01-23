6fdece7c8e49d72cd1229a7364381d38603bdc9f
usuario1 <usuariogmail.com>
2024-01-23 11:30:11 +0100

    primer envio

---
+++ js/codigo.js
@@ -0,0 +1,21 @@
+function saludar(){
+    let nombre="juan";
+    window.alert("hola, qué tal "+nombre);
+
+}
+
+function saludar2(nombre){
+    window.alert("hola, qué tal "+nombre);
+
+}
+
+function calcular(){
+    let precio=12;
+    let unidades=window.prompt("dime cuantas unidades");
+    let total=precio*unidades;
+    window.alert("el total es "+total);
+    window.confirm("Aceptas la compra");
+    
+
+
+}
